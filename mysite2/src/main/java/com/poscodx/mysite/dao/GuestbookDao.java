cb0175ed89ef8e24054ef820fe764a21884ab5e4
genieu99 <yujin0419@sookmyung.ac.kr>
2024-05-24 16:04:42 +0900

    class: mysite user #2

--- mysite2/src/main/java/com/poscodx/mysite/dao/GuestbookDao.java
+++ mysite2/src/main/java/com/poscodx/mysite/dao/GuestbookDao.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import guestbook.vo.GuestbookVo;
+import com.poscodx.mysite.vo.GuestbookVo;
 
 public class GuestbookDao {
 	
