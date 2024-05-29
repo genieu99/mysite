d05b842c5cb2df67d680ae543f04a2fef9b164a0
genieu99 <yujin0419@sookmyung.ac.kr>
2024-05-29 09:55:26 +0900

    class: servlet context's component #finish

--- mysite2/src/main/java/com/poscodx/mysite/controller/MainServlet.java
+++ mysite2/src/main/java/com/poscodx/mysite/controller/MainServlet.java
@@ -1,9 +1,20 @@
 package com.poscodx.mysite.controller;
 
+import javax.servlet.ServletException;
+
 import com.poscodx.mysite.controller.action.main.MainAction;
 
 public class MainServlet extends ActionServlet {
+	
 	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void init() throws ServletException {
+		String config = this.getServletConfig().getInitParameter("config");
+		System.out.println("MainController.init() called: " + config);
+		
+		super.init();
+	}
 
 	@Override
 	protected Action getAction(String actionName) {
