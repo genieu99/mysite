b1d681e2a9759077d45a7dde05ec9ff73cc92bfa
genieu99 <yujin0419@sookmyung.ac.kr>
2024-06-05 13:49:57 +0900

    class: mybatis #2

--- mysite3/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
+++ mysite3/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
@@ -1,12 +1,7 @@
 package com.poscodx.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
@@ -21,39 +16,9 @@ public class GuestbookRepository {
 	public GuestbookRepository(SqlSession sqlSession) {
 		this.sqlSession = sqlSession;
 	}
-
-	private Connection getConnection() throws SQLException {
-		Connection conn = null;
-		
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-			
-			String url = "jdbc:mariadb://192.168.64.3:3306/webdb?charset=utf8";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패: " + e);
-		}
-		
-		return conn;
-	}
 	
-	public void insert(GuestbookVo mockGuestbookVo) {
-		try (
-				Connection conn = getConnection();
-				PreparedStatement pstmt1 = conn.prepareStatement("insert into guestbook(name, password, contents, reg_date) values(?, ?, ?, now())");
-				PreparedStatement pstmt2 = conn.prepareStatement("select last_insert_id() from dual");
-		) {
-			pstmt1.setString(1, mockGuestbookVo.getName());
-			pstmt1.setString(2, mockGuestbookVo.getPassword());
-			pstmt1.setString(3, mockGuestbookVo.getContents());
-			pstmt1.executeUpdate();
-		
-			ResultSet rs = pstmt2.executeQuery();
-			mockGuestbookVo.setNo(rs.next() ? rs.getLong(1) : null);
-			rs.close();
-		} catch (SQLException e) {
-			System.out.println("error: " + e);
-		}
+	public void insert(GuestbookVo vo) {
+		sqlSession.insert("guestbook.insert", vo);
 	}
 	
 	public List<GuestbookVo> findAll() {
@@ -61,15 +26,6 @@ public class GuestbookRepository {
 	}
 
 	public void deleteByNoAndPassword(Long no, String password) {
-		try (
-				Connection conn = getConnection();
-				PreparedStatement pstmt = conn.prepareStatement("delete from guestbook where no = ? and password = ?");
-		) {
-			pstmt.setLong(1, no);
-			pstmt.setString(2, password);
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println("error: " + e);
-		}
+		sqlSession.delete("guestbook.deleteByNoAndPassword", Map.of("no", no, "password", password));
 	}
 }
