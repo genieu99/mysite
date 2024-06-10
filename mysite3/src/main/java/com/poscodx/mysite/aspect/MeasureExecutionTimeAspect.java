0d6ec40420550f00a91a0c7882db767b68b60bc5
genieu99 <yujin0419@sookmyung.ac.kr>
2024-06-10 16:13:21 +0900

    class: security AuthInterceptor #1

--- mysite3/src/main/java/com/poscodx/mysite/aspect/MeasureExecutionTimeAspect.java
+++ mysite3/src/main/java/com/poscodx/mysite/aspect/MeasureExecutionTimeAspect.java
@@ -9,7 +9,7 @@ import org.springframework.util.StopWatch;
 @Component
 @Aspect
 public class MeasureExecutionTimeAspect {
-	@Around("execution(* *..*.repository.*.*(..)) || execution(* *..*.*.service.*.*(..))")
+	@Around("execution(* *..*.repository.*.*(..)) || execution(* *..*.*.service.*.*(..)) || execution(* *..*.controller.*.*(..))")
 	public Object adviceAround(ProceedingJoinPoint pjp) throws Throwable {
 		// before
 		StopWatch sw = new StopWatch();
