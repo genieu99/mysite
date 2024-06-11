46d3bbdd82af49937993c255181c5c4b9f9a7592
genieu99 <yujin0419@sookmyung.ac.kr>
2024-06-11 09:56:22 +0900

    class: security argumentResolver + @authuser

--- mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.poscodx.mysite.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,7 +24,6 @@ public class UserController {
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
 	public String join(UserVo vo) {
-		System.out.println(vo);
 		userService.join(vo);
 		return "redirect:/user/joinsuccess";
 	}
@@ -44,7 +41,6 @@ public class UserController {
 	@Auth
 	@RequestMapping(value="/update", method=RequestMethod.GET)
 	public String update(@AuthUser UserVo authUser, Model model) {
-		// UserVo authUser = (UserVo)session.getAttribute("authUser");
 		UserVo vo = userService.getUser(authUser.getNo());
 		model.addAttribute("userVo", vo);
 		
@@ -52,11 +48,7 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/update", method=RequestMethod.POST)
-	public String update(HttpSession session, UserVo vo) {
-		UserVo authUser = (UserVo) session.getAttribute("authUser");
-		if (authUser == null) {
-			return "redirect:/";
-		}
+	public String update(@AuthUser UserVo authUser, UserVo vo) {
 		vo.setNo(authUser.getNo());
 		userService.update(vo);
 		authUser.setName(vo.getName());
