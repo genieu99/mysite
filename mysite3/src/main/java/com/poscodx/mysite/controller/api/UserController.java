ffd2f4750bf288b88c99b4d8bf6cb46e8d6ef2da
genieu99 <yujin0419@sookmyung.ac.kr>
2024-06-17 10:48:42 +0900

    class: JsonResult #1

--- mysite3/src/main/java/com/poscodx/mysite/controller/api/UserController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/api/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.poscodx.mysite.dto.JsonResult;
 import com.poscodx.mysite.service.UserService;
 import com.poscodx.mysite.vo.UserVo;
 
@@ -18,8 +19,12 @@ public class UserController {
 	private UserService userService;
 	
 	@RequestMapping("/checkemail")
-	public Object checkEmail(@RequestParam(value="email", required=true, defaultValue="") String email) {
+	public JsonResult checkEmail(@RequestParam(value="email", required=true, defaultValue="") String email) {
 		UserVo vo = userService.getUser(email);
-		return Map.of("exist", vo != null);
+		JsonResult jsonResult = new JsonResult();
+		
+		jsonResult.setResult("");
+		
+		return jsonResult;
 	}
 }
