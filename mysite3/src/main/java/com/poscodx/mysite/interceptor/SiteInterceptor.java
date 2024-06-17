46e62714afb2f2d8515820805d1f0224493ff514
genieu99 <yujin0419@sookmyung.ac.kr>
2024-06-17 14:23:12 +0900

    class: locale #1

--- mysite3/src/main/java/com/poscodx/mysite/interceptor/SiteInterceptor.java
+++ mysite3/src/main/java/com/poscodx/mysite/interceptor/SiteInterceptor.java
@@ -3,19 +3,20 @@ package com.poscodx.mysite.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.LocaleResolver;
 
 import com.poscodx.mysite.service.SiteService;
 import com.poscodx.mysite.vo.SiteVo;
 
 public class SiteInterceptor implements HandlerInterceptor {
 	
+	private LocaleResolver localeResolver;
+	
+	@Autowired
 	private SiteService siteService;
 	
-	public SiteInterceptor(SiteService siteService) {
-		this.siteService = siteService;
-	}
-
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		SiteVo siteVo = (SiteVo)request.getServletContext().getAttribute("siteVo");
@@ -23,6 +24,9 @@ public class SiteInterceptor implements HandlerInterceptor {
 			siteVo = siteService.getSite();
 			request.getServletContext().setAttribute("siteVo", siteVo);
 		}
+		
+		// Locale
+		System.out.println("resolver-locale: " + localeResolver.resolveLocale(request).getLanguage());
 		return true;
 	}
 
